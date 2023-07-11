602f9c3f5c0933b6d0f007d878c4b21db96a909d
Dinesh Kumar <dinekkum@dinekkum-mac.lan>
2023-07-11 02:02:55 -0400

    added homework 4

---
+++ homework4/homework/utils.py
@@ -0,0 +1,134 @@
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from . import dense_transforms
+
+
+class DetectionSuperTuxDataset(Dataset):
+    def __init__(self, dataset_path, transform=dense_transforms.ToTensor(), min_size=20):
+        from glob import glob
+        from os import path
+        self.files = []
+        for im_f in glob(path.join(dataset_path, '*_im.jpg')):
+            self.files.append(im_f.replace('_im.jpg', ''))
+        self.transform = transform
+        self.min_size = min_size
+
+    def _filter(self, boxes):
+        if len(boxes) == 0:
+            return boxes
+        return boxes[abs(boxes[:, 3] - boxes[:, 1]) * abs(boxes[:, 2] - boxes[:, 0]) >= self.min_size]
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        import numpy as np
+        b = self.files[idx]
+        im = Image.open(b + '_im.jpg')
+        nfo = np.load(b + '_boxes.npz')
+        data = im, self._filter(nfo['karts']), self._filter(nfo['bombs']), self._filter(nfo['pickup'])
+        if self.transform is not None:
+            data = self.transform(*data)
+        return data
+
+
+def load_detection_data(dataset_path, num_workers=0, batch_size=32, **kwargs):
+    dataset = DetectionSuperTuxDataset(dataset_path, **kwargs)
+    return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=True)
+
+class ConfusionMatrix(object):
+    def _make(self, preds, labels):
+        label_range = torch.arange(self.size, device=preds.device)[None, :]
+        preds_one_hot, labels_one_hot = _one_hot(preds, self.size), _one_hot(labels, self.size)
+        return (labels_one_hot[:, :, None] * preds_one_hot[:, None, :]).sum(dim=0).detach()
+
+    def __init__(self, size=5):
+        """
+        This class builds and updates a confusion matrix.
+        :param size: the number of classes to consider
+        """
+        self.matrix = torch.zeros(size, size)
+        self.size = size
+
+    def add(self, preds, labels):
+        """
+        Updates the confusion matrix using predictions `preds` (e.g. logit.argmax(1)) and ground truth `labels`
+        """
+        self.matrix = self.matrix.to(preds.device)
+        self.matrix += self._make(preds, labels).float()
+
+    @property
+    def class_iou(self):
+        true_pos = self.matrix.diagonal()
+        return true_pos / (self.matrix.sum(0) + self.matrix.sum(1) - true_pos + 1e-5)
+
+    @property
+    def iou(self):
+        return self.class_iou.mean()
+
+    @property
+    def global_accuracy(self):
+        true_pos = self.matrix.diagonal()
+        return true_pos.sum() / (self.matrix.sum() + 1e-5)
+
+    @property
+    def class_accuracy(self):
+        true_pos = self.matrix.diagonal()
+        return true_pos / (self.matrix.sum(1) + 1e-5)
+
+    @property
+    def average_accuracy(self):
+        return self.class_accuracy.mean()
+
+    @property
+    def per_class(self):
+        return self.matrix / (self.matrix.sum(1, keepdims=True) + 1e-5)
+
+
+
+if __name__ == '__main__':
+    dataset = DetectionSuperTuxDataset('dense_data/train')
+    import torchvision.transforms.functional as F
+    from pylab import show, subplots
+    import matplotlib.patches as patches
+    import numpy as np
+
+    fig, axs = subplots(1, 2)
+    for i, ax in enumerate(axs.flat):
+        im, kart, bomb, pickup = dataset[100 + i]
+        ax.imshow(F.to_pil_image(im), interpolation=None)
+        for k in kart:
+            ax.add_patch(
+                patches.Rectangle((k[0] - 0.5, k[1] - 0.5), k[2] - k[0], k[3] - k[1], fc='none', ec='r', lw=2))
+        for k in bomb:
+            ax.add_patch(
+                patches.Rectangle((k[0] - 0.5, k[1] - 0.5), k[2] - k[0], k[3] - k[1], fc='none', ec='g', lw=2))
+        for k in pickup:
+            ax.add_patch(
+                patches.Rectangle((k[0] - 0.5, k[1] - 0.5), k[2] - k[0], k[3] - k[1], fc='none', ec='b', lw=2))
+        ax.axis('off')
+    dataset = DetectionSuperTuxDataset('dense_data/train',
+                                       transform=dense_transforms.Compose([dense_transforms.RandomHorizontalFlip(0),
+                                                                           dense_transforms.ToTensor()]))
+    fig.tight_layout()
+    # fig.savefig('box.png', bbox_inches='tight', pad_inches=0, transparent=True)
+
+    fig, axs = subplots(1, 2)
+    for i, ax in enumerate(axs.flat):
+        print(f'i= {i}')
+        im, *dets = dataset[100 + i]
+        for s in range(3):
+            imc = im[s]
+            print(f' at s {s} im {imc.shape}')
+        hm, size = dense_transforms.detections_to_heatmap(dets, im.shape[1:])
+        ax.imshow(F.to_pil_image(im), interpolation=None)
+        hm = hm.numpy().transpose([1, 2, 0])
+        alpha = 0.25 * hm.max(axis=2) + 0.75
+        r = 1 - np.maximum(hm[:, :, 1], hm[:, :, 2])
+        g = 1 - np.maximum(hm[:, :, 0], hm[:, :, 2])
+        b = 1 - np.maximum(hm[:, :, 0], hm[:, :, 1])
+        ax.imshow(np.stack((r, g, b, alpha), axis=2), interpolation=None)
+        ax.axis('off')
+    fig.tight_layout()
+    # fig.savefig('heat.png', bbox_inches='tight', pad_inches=0, transparent=True)
+    show()
